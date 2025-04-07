9cee2782936a41f8249837c62ef96259c0473ded
Shorabh Singh <157907232+shorabh9@users.noreply.github.com>
2025-04-08 00:49:21 +0530

    Fix: case-sensitive import path for userSlice

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import {Link, NavLink} from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { authenticateUser, logoutUser } from '../reduxSlices/userSlice';
+import { authenticateUser, logoutUser } from '../reduxSlices/UserSlice';
 import Menu from './Menu';
 
 
